0cc20a4c06c361a4a95c16a5d390f4d714d74474
sashiniliyanage <e17190@eng.pdn.ac.lk>
2023-02-17 09:22:07 +0530

    update file path

--- file-read/TraversePack/src/main/java/org/wso2/internal/apps/license/manager/TraversePack.java
+++ file-read/TraversePack/src/main/java/org/wso2/internal/apps/license/manager/TraversePack.java
@@ -8,20 +8,22 @@ import java.nio.file.Paths;
 public class TraversePack {
 
     public static String addFile(String path, String name){
-    
+
         try {
+            Files.createDirectories(Paths.get("/tmp/Storage"));
             Files.copy( new URL(path).openStream(),
-                Paths.get("Storage/"+name));
-                return ("done");
-            } catch (Exception e) {
-                return (e.toString());
-            }
+            Paths.get("/tmp/Storage/"+name));
+            return ("done");
+        } catch (Exception e) {
+            return (e.toString());
+        }
+    
     }
 
     public static String checkFile(String name){
     
         try {
-            File f = new File("Storage/"+name);
+            File f = new File("/tmp/Storage/"+name);
             if(f.exists()){
                 return ("exists");
             }else{
